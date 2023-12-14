9a0d1e95a0016cd26e18855d1530ae6e3ff7209b
Ladislas Dellinger <111739019+ladislasdellinger@users.noreply.github.com>
2023-12-14 11:50:25 +0100

    move utility types

--- calculator.ts
+++ calculator.ts
@@ -1,17 +1,10 @@
-type TupleOfLength<
-  N extends number,
-  T extends any[] = []
-> = T["length"] extends N ? T : TupleOfLength<N, [...T, []]>;
+import { Concat, Pop, LengthOfArray, TupleOfLength } from "./utils";
 
-type Concat<T1 extends any[], T2 extends any[]> = [...T1, ...T2];
-type Pop<T extends any[]> = T extends [...infer Head, any] ? Head : never;
-type LengthOfArray<T extends any[]> = T["length"] & number;
-
-export type Add<N1 extends number, N2 extends number> = LengthOfArray<
+type Add<N1 extends number, N2 extends number> = LengthOfArray<
   Concat<TupleOfLength<N1>, TupleOfLength<N2>>
 >;
 
-export type Subtract<
+type Subtract<
   N1 extends number,
   N2 extends number,
   T1 extends any[] = TupleOfLength<N1>,
@@ -24,9 +17,11 @@ export type Subtract<
   ? never
   : Subtract<LengthOfArray<Pop<T1>>, LengthOfArray<Pop<T2>>>;
 
-export type Multiply<
+type Multiply<
   N1 extends number,
   N2 extends number,
   Result extends number = 0,
   I extends number = 0
 > = I extends N2 ? Result : Multiply<N1, N2, Add<Result, N1>, Add<I, 1>>;
+
+export { Add, Multiply, Subtract };
--- utils.ts
+++ utils.ts
@@ -1,7 +1,18 @@
-export type Equals<X, Y> = (<T>() => T extends X ? 1 : 2) extends <
-  T
->() => T extends Y ? 1 : 2
+type Equals<X, Y> = (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y
+  ? 1
+  : 2
   ? true
   : false;
 
-export type Assert<T extends true> = true;
+type Assert<T extends true> = T;
+
+type TupleOfLength<
+  N extends number,
+  T extends any[] = []
+> = T["length"] extends N ? T : TupleOfLength<N, [...T, []]>;
+
+type Concat<T1 extends any[], T2 extends any[]> = [...T1, ...T2];
+type Pop<T extends any[]> = T extends [...infer Head, any] ? Head : never;
+type LengthOfArray<T extends any[]> = T["length"] & number;
+
+export { Equals, Assert, Concat, Pop, LengthOfArray, TupleOfLength };
