e9708a2d7d1d2d54f313454df6abb1569cf6a4ea
Ladislas Dellinger <111739019+ladislasdellinger@users.noreply.github.com>
2023-12-14 11:48:17 +0100

    refactor substract

--- calculator.ts
+++ calculator.ts
@@ -5,7 +5,7 @@ type TupleOfLength<
 
 type Concat<T1 extends any[], T2 extends any[]> = [...T1, ...T2];
 type Pop<T extends any[]> = T extends [...infer Head, any] ? Head : never;
-type LengthOfArray<T extends any[]> = T["length"];
+type LengthOfArray<T extends any[]> = T["length"] & number;
 
 export type Add<N1 extends number, N2 extends number> = LengthOfArray<
   Concat<TupleOfLength<N1>, TupleOfLength<N2>>
@@ -14,21 +14,19 @@ export type Add<N1 extends number, N2 extends number> = LengthOfArray<
 export type Subtract<
   N1 extends number,
   N2 extends number,
-  T1 = TupleOfLength<N1>,
-  T2 = TupleOfLength<N2>
+  T1 extends any[] = TupleOfLength<N1>,
+  T2 extends any[] = TupleOfLength<N2>
 > = T2 extends []
   ? N1
-  : T2 extends [infer Head2, ...infer Rest2]
-  ? T1 extends [infer Head1, ...infer Rest1]
-    ? Subtract<Rest1["length"], Rest2["length"]>
-    : never
-  : never;
+  : Pop<T1> extends never
+  ? never
+  : Pop<T2> extends never
+  ? never
+  : Subtract<LengthOfArray<Pop<T1>>, LengthOfArray<Pop<T2>>>;
 
 export type Multiply<
   N1 extends number,
   N2 extends number,
   Result extends number = 0,
   I extends number = 0
-> = I extends N2
-  ? Result
-  : Multiply<N1, N2, Add<Result, N1> & number, Add<I, 1> & number>;
+> = I extends N2 ? Result : Multiply<N1, N2, Add<Result, N1>, Add<I, 1>>;
